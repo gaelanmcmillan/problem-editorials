d8d5626251199172b8a806393bf408c4fe8ddc76
Gaelan McMillan <73502924+gaelanmcmillan@users.noreply.github.com>
2022-03-28 18:51:55 -0400

    fix indentation

--- awkwardparty/awkwardparty.cpp
+++ awkwardparty/awkwardparty.cpp
@@ -5,17 +5,18 @@
  * Solution by Gaelan McMillan
  * 
  * Problem: 
- * 		Given a list of integers,
- * 		find the "awkwardness" of the list,
- * 		where "awkwardness" is defined as
- * 		the distance between the two closest
- * 		same-value integers.
+ * 	Given a list of integers,
+ * 	find the "awkwardness" of the list,
+ * 	where "awkwardness" is defined as
+ * 	the distance between the two closest
+ * 	same-value integers.
  * 
- * Solution: "Online" min distance between two closest integers
- * 	1. Read in integers, keeping track of the position each value was last seen in.
- *  2. If the current integer has been seen already, 
- *     take its distance from its last occurence and
- * 	   compare it against the current minimum.
+ * Solution:
+ *	"Online" min distance between two closest integers
+ * 	1.	Read in integers, keeping track of the position each value was last seen in.
+ *  	2. 	If the current integer has been seen already, 
+ *     		take its distance from its last occurence and
+ * 		compare it against the current minimum.
  *
  * Complexity: O(n)
 */
